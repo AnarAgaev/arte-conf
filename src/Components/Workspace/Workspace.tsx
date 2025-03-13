06999f09a5dbed6a52213318bee7e388913b438f
Anar Agaev <anar.n.agaev@gmail.com>
2025-03-13 14:10:43 +0300

    Added workspace buttn

--- src/Components/Workspace/Workspace.tsx
+++ src/Components/Workspace/Workspace.tsx
@@ -1,7 +1,7 @@
 import { Steps } from '@components'
 import style from './Workspace.module.sass'
 
-const { workspace } = style
+const { workspace, buttons } = style
 
 export const Workspace = () => {
 
@@ -14,7 +14,15 @@ export const Workspace = () => {
             <Steps />
 
             <div>Workspace body</div>
-            <div>Workspace footer --- buttons</div>
+
+            <div className={buttons}>
+                <button type='button' className='btn btn_small btn_block btn_lite'>
+                    сбросить все
+                </button>
+                <button type='button' className='btn btn_small btn_block btn_dark'>
+                    следующий шаг
+                </button>
+            </div>
         </div>
     )
 }
