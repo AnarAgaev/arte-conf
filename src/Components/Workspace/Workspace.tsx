fce01f586cd62708fddd9e56507b612828fbb369
Anar Agaev <anar.n.agaev@gmail.com>
2025-03-13 17:41:02 +0300

    Added buttons to the Workspace

--- src/Components/Workspace/Workspace.tsx
+++ src/Components/Workspace/Workspace.tsx
@@ -16,10 +16,10 @@ export const Workspace = () => {
             <div>Workspace body</div>
 
             <div className={buttons}>
-                <button type='button' className='btn btn_small btn_block btn_lite'>
+                <button type='button' className='btn btn_small btn_lite'>
                     сбросить все
                 </button>
-                <button type='button' className='btn btn_small btn_block btn_dark'>
+                <button type='button' className='btn btn_small btn_dark'>
                     следующий шаг
                 </button>
             </div>
